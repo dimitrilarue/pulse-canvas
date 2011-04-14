bd9c64962cca311008a7f25b8de82c0ce1838352
Dimitri Larue <siderax@gmail.com>
2011-04-14 15:08:07 +0200

    Simple optimisation

--- core/loader.js
+++ core/loader.js
@@ -1,11 +1,9 @@
-function (pulse){
+(function (pulse){
 
     var images = {},
         loader = {};
     
-    loader.addImage = function () {
-        var src  = arguments[0],
-            onload = arguments[1];
+    loader.addImage = function (src, onload) {
             
         if(typeof src !== "string") throw 'Source url of the image must be a string';
         if(images[src] instanceof HTMLImageElement === false) {
@@ -18,11 +16,11 @@ function (pulse){
     };
     
     loader.getImage = function (src) {
-        if(images[src] instanceof Image) return images[src];
+        if(images[src] instanceof HTMLImageElement) return images[src];
         return false;
     };
     
     
     pulse.loader = loader;
     
-}(pulse);
+})(pulse);
